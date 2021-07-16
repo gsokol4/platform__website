19c6c42039f9e46ae87d993e8c6ba4e2bf66c6cf
allsgood1 <59664235+allsgood1@users.noreply.github.com>
2021-07-15 23:15:03 -0500

    Update StyledBioBox.js

--- src/components/modules/UserProfile/BioBox/StyledBioBox.js
+++ src/components/modules/UserProfile/BioBox/StyledBioBox.js
@@ -3,6 +3,8 @@ import Styled from "styled-components";
 export const Wrapper = Styled.div`
 `;
 export const Bio = Styled.textarea`
+border: none;
 resize: none;
 width: 99%;
+background: #f0f0f0;
 `;
